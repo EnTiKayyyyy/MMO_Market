e35a2d5ce385e06b01e8b32906bf75d122a68656
EnTiKayyyyy <91786405+EnTiKayyyyy@users.noreply.github.com>
2025-06-02 13:11:44 +0700

    update

--- SourceCode/mmo-ecommerce-backend/server.js
+++
@@ -1,9 +0,0 @@
-// server.js
-const app = require('./src/app'); // Import app instance từ app.js
-require('dotenv').config(); // Đảm bảo biến môi trường được load ở entry point
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
--- SourceCode/mmo-ecommerce-backend/src/middleware/asyncHandler.js
+++
@@ -1,8 +0,0 @@
-// src/middleware/asyncHandler.js
-
-// Bắt lỗi từ các hàm async và chuyển nó đến middleware xử lý lỗi của Express
-const asyncHandler = (fn) => (req, res, next) => {
-    Promise.resolve(fn(req, res, next)).catch(next);
-  };
-  
-  module.exports = asyncHandler;
